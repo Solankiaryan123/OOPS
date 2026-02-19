43282079af5e8747abeeb92b0c3b1f1c65ad48f3
Solankiaryan123 <sa7216929@gmail.com>
2026-02-19 12:53:03 +0530

    Add files via upload

---
+++ practical10.java
@@ -0,0 +1,18 @@
+import java.util.canner;
+class Employee{
+private string EmployeeName
+private double EmployeeSalary
+
+void read Employeedata{
+
+}
+
+void displayemployeedata(){
+system.out.print("EmployeeName:"+EmployeeName);
+system.out.print("EmployeeSalary:"+EmployeeSalary);
+
+
+}
+
+
+
